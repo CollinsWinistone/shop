5223790ed44b95600672bd504e0d7d678c1cf7b3
Collins Simiyu <collinssimiyu85@gmail.com>
2020-11-05 04:12:31 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <!--  logo section -->
+    <div id="logo">
+        <h3>Dary Shop</h3>
+    </div>
+
+    <hr>
+
+    <!-- search bar -->
+    <div id="search_bar">
+        <p>Search Product</p>
+        <input type="search" placeholder="Search here">
+
+    </div>
+    
+    
+</body>
+</html>
