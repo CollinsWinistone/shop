a6ef727bb00c20cbe2ad3782b53421eef6679ae0
Collins Simiyu <collinssimiyu85@gmail.com>
2020-11-06 14:17:46 +0300

    made a quick styling

--- index.php
+++ index.php
@@ -4,28 +4,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="styles/index.css">
 </head>
 <body>
 
 
     <!--  logo section -->
     <div id="logo">
-        <h3>Dary Shop</h3>
+        <h3 class="inline">Dary Shop</h3>
+        <h3 class="inline" id="test">Logged in as Person</h3>
     </div>
 
-    <div id="logged_in_status">
-        Logged in as Entrepreneur
-
-    </div>
+    
 
     <hr>
 
-    <!-- search bar -->
-    <div id="search_bar">
-        <p>Search Product</p>
-        <input type="search" placeholder="Search here">
-
-    </div>
+    <!--- navigation bar-->
+    <nav>
+        <ul>
+            <li>Products</li>
+            <li>Statistics</li>
+            <li>Settings</li>
+        </ul>
+    </nav>
     
     
     
