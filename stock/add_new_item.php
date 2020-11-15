93bc864741007f69bda9baac944c516b59a366ab
Collins Simiyu <collinssimiyu85@gmail.com>
2020-11-15 09:55:18 +0300

    added stock page

---
+++ stock/add_new_item.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- start of introduction section-->
+    <section>
+        <blockquote>
+            "Add new item to help us keep track of your records"
+        </blockquote>
+    </section>
+    <!-- end of introduction section-->
+
+    <!--start of form records section-->
+
+    <form action="#">
+        <input type="text" name="item_name" placeholder="Item name"><br>
+        <input type="text" name="price_per_unit" placeholder="price per unit"><br>
+        <input type="text" name="unit" placeholder="unit"><br>
+        
+        <input type="submit">
+        
+    </form>
+
+    <!-- end of from records section-->
+</body>
+</html>
---
+++ stock/stock.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- start of introduction div-->
+    <div>
+        <blockquote>
+        "The information required helps us to give you the correct statistics"
+        </blockquote>
+    </div>
+
+    <!-- end of introduction div-->
+
+    <!-- start of new item-->
+    <a href="add_new_item.php">Add new item</a>
+
+
+    <!-- end of new itwm-->
+
+    <!-- capital section-->
+
+    <section>
+        <form action="#" method="POST">
+            <input type="text" name="capital">
+            <input type="submit" value="Submit">
+        </form>
+    </section>
+
+    <!-- end of capital section-->
+
+</body>
+</html>
