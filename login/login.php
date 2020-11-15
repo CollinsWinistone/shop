70b51b83d867dadc3e2cc26bb7763413884dd649
Collins Simiyu <collinssimiyu85@gmail.com>
2020-11-15 09:00:10 +0300

    added login script

---
+++ login/login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!--introduction-->
+    <div>
+        <blockquote>
+            Hey dear user.Please log in and find out how your business is 
+            growing larger and larger
+        </blockquote>
+    </div>
+
+    <!--end of introduction-->
+
+    <!-- start of login form -->
+    <form action="login_user.php" method="POST">
+        <input type="email" name="email" placeholder="email"><br>
+        <input type="password" name="password" placeholder="passowrd"><br>
+        <input type="submit" value="Login">
+    </form>
+    <!--end of login form -->
+</body>
+</html>
