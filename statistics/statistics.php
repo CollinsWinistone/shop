21ded3e50e13ba2973c934a98443f61d33d36929
Collins Simiyu <collinssimiyu85@gmail.com>
2020-11-17 13:24:34 +0300

    statistics

--- statistics/statistics.php
+++ statistics/statistics.php
@@ -1,14 +1,19 @@
 <!DOCTYPE html>
+<html lang="en">
 <html>
 <head>
-	<title></title>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>statistics</title>
+    <link rel="stylesheet" href="../styles/bootsrap/css/bootstrap.min.css">
+    <script src="../styles/fontawesome/js/all.js"></script>
 </head>
 <body>
     <!-- header-->
-
-    
-
+    <?php include "../include_files/header.php";?>
     <!-- end of header-->
 
+<script src="../js/jqery.js"></script>
+<script src="../styles/bootsrap/js/bootstrap.min.js"></script>
 </body>
 </html>
