73bb01bb9191f4cbd53e38d570e051c5427d4c36
Collins Simiyu <collinssimiyu85@gmail.com>
2020-11-18 16:03:26 +0300

    added edit file

---
+++ edit/edit.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../styles/index.css">
+    <link rel="stylesheet" href="../styles/header/header.css">
+    <link rel="stylesheet" href="../styles/bootsrap/css/bootstrap.min.css">
+    <script src="../styles/fontawesome/js/all.js"></script>
+    <title>Document</title>
+   
+</head>
+<body>
+    <!-- header -->
+    <?php include "../include_files/header.php"; ?>
+    <!-- end header-->
+
+    
+<script src="../js/jqery.js"></script>
+<script src="../styles/bootsrap/js/bootstrap.min.js"></script>
+</body>
+</html>
