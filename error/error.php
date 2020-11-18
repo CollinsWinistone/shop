1001d4e96b0f3a700378fe0237621bc5825bf9f9
Collins Simiyu <collinssimiyu85@gmail.com>
2020-11-18 14:29:47 +0300

    images

---
+++ error/error.php
@@ -0,0 +1,29 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../styles/header/header.css">
+    <link rel="stylesheet" href="../styles/bootsrap/css/bootstrap.min.css">
+    <script src="../styles/fontawesome/js/all.js"></script>
+    
+</head>
+<body>
+    <!-- header section-->
+
+    <?php include "../include_files/header.php"; ?>
+
+    <!-- end of header section-->
+
+  
+
+
+    
+
+
+<script src="../js/jqery.js"></script>
+<script src="../styles/bootsrap/js/bootstrap.min.js"></script>
+</body>
+</html>
