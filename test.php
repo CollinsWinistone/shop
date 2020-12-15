72c71fdbdf649c47c55f970aff06aaff5e4c9a68
Collins Simiyu <collinssimiyu85@gmail.com>
2020-12-15 20:09:25 +0300

    ....

--- test.php
+++ test.php
@@ -1,11 +1,31 @@
 <?php 
-include "database/dbc.php";
-//mysqli
+$stock = $_SERVER['DOCUMENT_ROOT']."/dary/lib/stock.php";
+$test=$_SERVER['DOCUMENT_ROOT']."/dary/config/config.php";
+$database=$_SERVER['DOCUMENT_ROOT']."/dary/lib/cosa_db.php";
+$user=$_SERVER['DOCUMENT_ROOT']."/dary/lib/user.php";
 
-$result = $mysqli->query("SELECT 'Collins is the best Engineer' AS test");
-$row = $result->fetch_assoc();
+include ("$test");
+include ("$database");
+include ("$stock");
+include ("$user");
 
-echo $row['test'];
+//$db=Database::connect(DB_HOST,DB_USERNAME,DB_NAME,DB_PASSWORD);
+$db=Database::connect_default();
+
+$reg_data=[
+    'email'=>'collinssimiyu85@gmail.com',
+    'contact'=>'0713080474',
+    'password'=>'salama',
+    'first_name'=>'debrah',
+    'last_name'=>'salama'
+
+];
+$test=new User;
+$test->login($db,$reg_data);
+
+
+
+
 
 
 
