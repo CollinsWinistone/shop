ffb346683faa4f1fc12d11bbf80ddd81f9717e19
Collins Simiyu <collinssimiyu85@gmail.com>
2021-01-14 20:37:43 +0300

    added profit folder

---
+++ profit/profit.php
@@ -0,0 +1,15 @@
+<?php
+
+$test=$_SERVER['DOCUMENT_ROOT']."/dary/lib/sales_stats.php";
+include "{$_SERVER['DOCUMENT_ROOT']}/dary/lib/cosa_db.php";
+include ("$test");
+
+$db = Database::connect_default();
+
+$app = new SalesStats;
+$profit = $app->getProfitOnSales($db,1);
+
+echo $profit['profit'];
+
+
+?>
