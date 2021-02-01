a4e414ab25a314b3aca3454abb5f66d0290fed4d
collins simiyu <collinssimiyu85@gmail.com>
2021-02-01 17:07:13 +0300

    .....

--- test.php
+++ test.php
@@ -4,10 +4,8 @@ $test=$_SERVER['DOCUMENT_ROOT']."/shop/lib/sales_stats.php";
 include "{$_SERVER['DOCUMENT_ROOT']}/shop/lib/cosa_db.php";
 include ("$test");
 include "{$_SERVER['DOCUMENT_ROOT']}/shop/lib/validator.php";
-// database connectivity
+
 $db = Database::connect_default();
-
-
 
 
 
