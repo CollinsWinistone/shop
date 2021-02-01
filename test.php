dc733b8d3afb36b63375262b448c0f56ad333a73
collins simiyu <collinssimiyu85@gmail.com>
2021-02-01 17:01:01 +0300

    added comment for database connectivty

--- test.php
+++ test.php
@@ -4,7 +4,10 @@ $test=$_SERVER['DOCUMENT_ROOT']."/shop/lib/sales_stats.php";
 include "{$_SERVER['DOCUMENT_ROOT']}/shop/lib/cosa_db.php";
 include ("$test");
 include "{$_SERVER['DOCUMENT_ROOT']}/shop/lib/validator.php";
+// database connectivity
 $db = Database::connect_default();
+
+
 
 
 
